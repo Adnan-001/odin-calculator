f5cad91739072795d133b8106d1bc309e5974257
Adnan-001 <107484208+Adnan-001@users.noreply.github.com>
2022-07-25 16:21:58 +0500

    Populate display on numeric and decimal button press
    When user clicks decimal button or numeric button the relevant text
    is displayed on the main field.

--- script.js
+++ script.js
@@ -40,3 +40,47 @@ function operate(operator, num1, num2) {
 
     return result;
 }
+
+function appendCharToTextField(str, tf) {
+    tf.value = tf.value+str;
+}
+
+function addListenersToNumericBtns() {
+    const numericBtns = document.querySelectorAll('.btn.number button');
+    const mainTextField = document.querySelector('.calculator-display .main-field input');
+
+    numericBtns.forEach((btn) => {
+        btn.addEventListener('click', e => {
+            appendCharToTextField(e.target.textContent, mainTextField);
+        });
+    });
+}
+
+function addListenersToDecimalBtn() {
+    const decimalBtn = document.querySelector('.btn.deci button');
+
+    const mainTextField = document.querySelector('.calculator-display .main-field input');
+
+    decimalBtn.onclick = (e) => {
+        if (globalObj.decimalFlag) {
+            return;     // in-case there already is a decimal flag in text-field
+        }
+
+        mainTextField.value === '' ? 
+        appendCharToTextField('0.', mainTextField) :
+        appendCharToTextField('.', mainTextField);
+
+        globalObj.decimalFlag = true;
+    }
+}
+
+let globalObj = {
+    decimalFlag : false,
+    operatorFlag : false,
+    operand1 : null,
+    operand2 : null,
+
+};
+
+addListenersToNumericBtns();
+addListenersToDecimalBtn();
